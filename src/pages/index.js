598b93c9acf4d530b275337b656aa31c71739a2a
Tiago Fragoso <tiagofragoso@outlook.com>
2019-07-13 12:10:58 +0100

    Coming soon branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,17 @@
 import React from "react";
-import { graphql } from "gatsby";
-import PropTypes from "prop-types";
 
 import Header from "../components/header";
-import Bid from "../components/bid";
 import Content from "../components/content";
-import Timer from "../components/timer";
-import Update from "../components/update";
 
 import '../components/layout.css';
 
-const Index = ({data}) => (
+const Index = () => (
     <> 
         <Header />
         <Content>
-            <Bid data={data.allDataJson.edges[0].node.bid} />
-            <Timer data={data.allDataJson.edges[0].node.bid} />
-            <Update date={data.allDataJson.edges[0].node.update}/>
+            Brevemente disponível
         </Content>
     </>
 );
-
-Index.propTypes = {
-    data: PropTypes.any.isRequired
-};
-
-export const query = graphql`
-    query DataQuery {
-        allDataJson {
-        edges {
-            node {
-                bid {
-                value
-                companyId
-                date
-            } 
-            update
-            }
-        }
-        }
-    }  
-`;
 
 export default Index;
